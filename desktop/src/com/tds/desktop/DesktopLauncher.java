476e68df01dfe5970c9f220e94edeeb37afbf715
Keith <keith.drew.13@gmail.com>
2025-08-18 13:14:30 -0700

    Update LibGDX for Apple Silicon

--- desktop/src/com/tds/desktop/DesktopLauncher.java
+++ desktop/src/com/tds/desktop/DesktopLauncher.java
@@ -1,19 +1,16 @@
 package com.tds.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.Graphics.DisplayMode;
 import com.tds.TDS;
-import java.awt.Toolkit;
-import java.awt.Dimension;
 
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-                Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-                config.width = (int)screenSize.getWidth();
-                config.height = (int)screenSize.getHeight();
-                config.fullscreen = true;
-		new LwjglApplication(new TDS(), config);
-	}
+        public static void main (String[] arg) {
+                Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+                DisplayMode displayMode = Lwjgl3ApplicationConfiguration.getDisplayMode();
+                config.setFullscreenMode(displayMode);
+                new Lwjgl3Application(new TDS(), config);
+        }
 }
