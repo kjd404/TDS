17e565bebcf6b0d3ea36ce931dadaf4faad8eb6e
Keith <keith.drew.13@gmail.com>
2025-08-18 21:17:33 -0700

    Use display mode dimensions in desktop launcher

--- desktop/src/com/tds/desktop/DesktopLauncher.java
+++ desktop/src/com/tds/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.tds.desktop;
 
+import com.badlogic.gdx.Graphics.DisplayMode;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.tds.GameBootstrap;
@@ -9,8 +10,9 @@ import com.tds.screen.OrthographicRenderStrategy;
 public class DesktopLauncher {
     public static void main(String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        int width = 800;
-        int height = 600;
+        DisplayMode mode = Lwjgl3ApplicationConfiguration.getDisplayMode();
+        int width = mode.width;
+        int height = mode.height;
         config.setWindowedMode(width, height);
         TDS game = new GameBootstrap()
                 .withRenderStrategy(new OrthographicRenderStrategy(width, height))
